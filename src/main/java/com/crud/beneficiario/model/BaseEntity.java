c4813c594c2c0f4d6db5a9a01bbfd44d3a195026
bispobr <156684382+bispobr@users.noreply.github.com>
2026-04-18 10:13:03 -0300

    feat: add entidade base auditoria

---
+++ src/main/java/com/crud/beneficiario/model/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.crud.beneficiario.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "data_inclusao",updatable = false)
+    private LocalDateTime dataInclusao;
+
+    @LastModifiedDate
+    @Column(name = "data_atualização")
+    private LocalDateTime dataAlteração;
+
+
+}
