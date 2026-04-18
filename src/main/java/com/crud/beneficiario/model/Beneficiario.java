bf5beb243a02e9fd109eb6d05725149a262e7c9c
bispobr <156684382+bispobr@users.noreply.github.com>
2026-04-18 10:13:34 -0300

    feat: add classe beneficiario

---
+++ src/main/java/com/crud/beneficiario/model/Beneficiario.java
@@ -0,0 +1,28 @@
+package com.crud.beneficiario.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Beneficiario extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String telefone;
+    private LocalDate dataNascimento;
+
+    @OneToMany(mappedBy = "beneficiario", cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<Documento> documentos = new ArrayList<>();
+
+}
